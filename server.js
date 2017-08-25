809a5acc5a276a10d4f2db4851b02527c24212aa
Nimisha Jadav <31312530+nimijdv10@users.noreply.github.com>
2017-08-25 12:17:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req, res){
+   res.send('Article one send to the server'); 
+});
+
+app.get('/article-two',function(req, res){
+   res.send('Article two send to the server'); 
+});
+
+app.get('/article-three',function(req, res){
+   res.send('Article three send to the server'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
